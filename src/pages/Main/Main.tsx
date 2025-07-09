056a4be3b6430fe57e296170209ca0c8875c9606
BoYueChuang <otis29516368@gmail.com>
2025-07-09 13:29:01 +0800

    拔掉 register 頁面 ＋ 新增 main 頁面

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,8 @@
+export const Main: React.FC = () => {
+  // 組件邏輯
+  return (
+    <div>
+      <h1>Main</h1>
+    </div>
+  );
+};
--- src/pages/Register/Register.tsx
+++
@@ -1,8 +0,0 @@
-export const Register: React.FC = () => {
-  // 組件邏輯
-  return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  );
-};
